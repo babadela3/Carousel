14b13f45624b5f692e1118de22345ea1e237df4f
babadela3 <teosposib@yahoo.com>
2016-09-08 10:56:14 +0300

    Carousel

---
+++ app/src/main/java/com/example/teo/carousel/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.teo.carousel;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FeatureCoverFlow coverFlow;
+    private CoverFlowAdapter adapter;
+    private ArrayList<Game> games;
+    private int poz;
+    private ViewGroup parent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        coverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
+        settingDummyData();
+        adapter = new CoverFlowAdapter(this, games);
+        coverFlow.setAdapter(adapter);
+        coverFlow.setOnScrollPositionListener(onScrollListener());
+
+    }
+
+    private FeatureCoverFlow.OnScrollPositionListener onScrollListener() {
+        return new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                Log.v("MainActiivty", "position: " + position);
+                poz = position;
+            }
+
+            @Override
+            public void onScrolling() {
+                Log.i("MainActivity", "scrolling");
+            }
+        };
+    }
+
+    private void settingDummyData() {
+        games = new ArrayList<>();
+        games.add(new Game(R.mipmap.google, "Google +"));
+        games.add(new Game(R.mipmap.fb, "Facebook"));
+        games.add(new Game(R.mipmap.twitter, "Twitter"));
+        games.add(new Game(R.mipmap.instagram, "Instagram"));
+        games.add(new Game(R.mipmap.linkedin, "Linkedin"));
+        games.add(new Game(R.mipmap.ic_launcher, "Android"));
+
+    }
+}
